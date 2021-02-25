17d3bd2b672e3c46f32c487fdec76f2985763501
Jeevan Joseph <jeevan.g@gmail.com>
2021-02-24 17:05:13 -0800

    Added Headers

--- src/main/sql/todo.sql
+++ src/main/sql/todo.sql
@@ -1,3 +1,6 @@
+-- Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+-- Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
+
 CREATE TABLE `todos` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
--- src/main/webapp/edit.jsp
+++ src/main/webapp/edit.jsp
@@ -1,4 +1,5 @@
 <!-- Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved. -->
+<!-- Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl. -->
 
 <%@ page language="java" contentType="text/html; charset=UTF-8"
   pageEncoding="UTF-8"%>
